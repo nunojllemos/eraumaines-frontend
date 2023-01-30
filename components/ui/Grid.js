405202f13b92750b1b90ab1a24a38fecfff164f1
Nuno Lemos <nuno@v-a.pt>
2023-01-30 09:27:12 +0000

    Some tests

---
+++ components/ui/Grid.js
@@ -0,0 +1,5 @@
+const Grid = ({ children }) => {
+    return <div className='grid grid-cols-4 640:grid-cols-12 gap-x-8'>{children}</div>
+}
+
+export default Grid
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        domains: ['source.unsplash.com'],
+        deviceSizes: [380, 640, 750, 1080, 1920, 3840],
+    },
 }
 
 module.exports = nextConfig
