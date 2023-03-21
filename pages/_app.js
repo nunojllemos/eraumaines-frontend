ba5a3b7c0c8452ce1cc89b1ab9b72c5a9682f653
Nuno Lemos <nuno@v-a.pt>
2023-03-21 14:47:25 +0000

    Test commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '/styles/globals.scss'
-import Header from 'components/blocks/Header'
+import Header from '/components/blocks/Header'
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Header />
+			{console.log('test')}
 			<Component {...pageProps} />
 		</>
 	)
