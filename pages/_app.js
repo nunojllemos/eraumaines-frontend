9356e6e37bfeeb2cb3763cb068a28d01cd1ad2d4
Nuno Lemos <nuno@v-a.pt>
2023-03-22 17:47:42 +0000

    Added alert to dev mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { useEffect } from 'react'
 import '@/styles/globals.scss'
 import Header from '@/organisms/Header'
 
 export default function App({ Component, pageProps }) {
+	useEffect(() => {
+		alert('Development branch')
+	}, [])
+
 	return (
 		<>
 			<Header />
