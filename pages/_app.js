e1ed9218f0b09c69af7dc236f19628ca51770487
Nuno Lemos <nuno@v-a.pt>
2023-03-22 17:42:23 +0000

    Some fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '@/styles/globals.scss'
-
-import Header from '@/components/blocks/Header'
+import Header from '@/organisms/Header'
 
 export default function App({ Component, pageProps }) {
 	return (
