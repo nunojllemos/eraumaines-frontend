5b69dc1bfa2196ed0fe208d75f248eaf8fbe9410
Nuno Lemos <nuno@v-a.pt>
2023-03-23 15:35:40 +0000

    Add Seo component

---
+++ components/atoms/Seo.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+const Seo = ({ title = 'Era Uma Inês', description = 'Website de fotografia e vídeo expecializado em eventos de casamento.' }) => {
+	return (
+		<Head>
+			<title>{title}</title>
+			<meta name='description' content={description} />
+		</Head>
+	)
+}
+export default Seo
