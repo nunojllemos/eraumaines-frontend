184df208bb3c33e60b8e54b0d8704f652ff84840
Nuno Lemos <nuno@v-a.pt>
2023-03-23 14:08:43 +0000

    Add api call in index

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Container from '@/layout/Container'
 import Col from '@/layout/Col'
 import Grid from '@/layout/Grid'
 
-export default function Home() {
+export default function Home({ data }) {
+	const { hero_title } = data.attributes
+
 	return (
 		<>
 			<Head>
@@ -14,6 +16,7 @@ export default function Home() {
 			</Head>
 			<main className='py-40'>
 				<Container>
+					<h1>{hero_title}</h1>
 					<Grid mobileCols={2} tabletCols={12} desktopCols={12}>
 						<Col>1</Col>
 					</Grid>
@@ -21,4 +24,14 @@ export default function Home() {
 			</main>
 		</>
 	)
+}
+
+export async function getStaticProps(context) {
+	const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/home`)
+		.then(res => res.json())
+		.then(data => data)
+
+	return {
+		props: data,
+	}
 }
