c88f0332d4e1869639ea04a5aafbaf04e051a213
Nuno Lemos <nuno@v-a.pt>
2023-04-12 14:51:52 +0100

    Finish AnimatedTitle scroll animation and change Subjectivity font file to Light.

--- components/atoms/AnimatedTitleText.js
+++ components/atoms/AnimatedTitleText.js
@@ -1,23 +1,21 @@
-import { useState, useEffect } from 'react'
-import { useScroll, motion } from 'framer-motion'
+import { useRef } from 'react'
+import { useScroll, motion, useTransform } from 'framer-motion'
 
 const AnimatedTitleText = ({ isPrimary, children }) => {
-    const { scrollY } = useScroll()
-    const [scrollValue, setScrollValue] = useState(0)
-
-    useEffect(() => {
-        // console.log(scrollYProgress)
-        // setScrollValue(scrollY.get())
-        // console.log('s: ', scrollValue)
-    }, [scrollValue, scrollY])
+    const targetRef = useRef()
+    const { scrollYProgress } = useScroll({
+        target: targetRef,
+        offset: ['end start', 'start end'],
+    })
+    const x = useTransform(scrollYProgress, [0, 1], ['-100vh', '0px'])
 
     return (
         <motion.div
-            style={{ translateX: `${scrollY.get()}px`, translateY: '-10%' }}
+            ref={targetRef}
+            style={{ translateY: '-10%', x }}
             className='leading-[1.3] text-80 768:text-100 1280:text-130 font-maginia whitespace-nowrap'
         >
             {isPrimary ? <h1>{children}</h1> : <h2>{children}</h2>}
-            {/* <motion.div className='h-2 w-24 bg-[red]' style={{ scaleX: scrollYProgress }}></motion.div> */}
         </motion.div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -65,6 +65,10 @@ module.exports = {
             150: '15rem',
         },
 
+        transitionTimingFunction: {
+            DEFAULT: 'cubic-bezier(0.19, 1, 0.22, 1)',
+        },
+
         extend: {
             fontFamily: {
                 subjectivity: 'Subjectivity',
