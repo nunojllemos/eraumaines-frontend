ef3e3564d16c5d8b8d6554b744d7040b3d8e859e
Nuno Lemos <nuno@v-a.pt>
2023-04-18 19:15:40 +0100

    WIP: Nos page

--- pages/diario/index.js
+++ pages/diario/index.js
@@ -12,95 +12,97 @@ const Diario = ({ data }) => {
 
     return (
         <main className='pb-16'>
-            <AnimatedTitle>{`${t.diary.title} . ${t.diary.title} . ${t.diary.title} . ${t.diary.title} . ${t.diary.title} . `}</AnimatedTitle>
+            <AnimatedTitle>{`${t.diary.title} . ${t.diary.title} . ${t.diary.title} . ${t.diary.title} . ${t.diary.title} . ${t.diary.title} .`}</AnimatedTitle>
             <Container>
-                <Grid rowGap={2}>
-                    <Col mobileCols={2} tabletCols={8}>
-                        <div className='flex flex-col gap-8'>
-                            {data[0] && (
-                                <DiaryCard
-                                    src={data[0].attributes?.cover?.data?.attributes?.url}
-                                    aspectRatio='16/9'
-                                    title={data[0].attributes?.title}
-                                    description={data[0].attributes?.description}
-                                    href={data[0].attributes?.slug}
-                                />
-                            )}
-                            {data[1] && (
-                                <DiaryCard
-                                    src={data[1].attributes?.cover?.data?.attributes?.url}
-                                    aspectRatio='16/9'
-                                    title={data[1].attributes?.title}
-                                    description={data[1].attributes?.description}
-                                    href={data[1].attributes?.slug}
-                                />
-                            )}
-                        </div>
-                    </Col>
-
-                    <Col mobileCols={2} tabletCols={4}>
-                        <div className='flex flex-col gap-8 h-full'>
-                            {data[2] && (
-                                <DiaryCard
-                                    src={data[2].attributes?.cover?.data?.attributes?.url}
-                                    aspectRatio='4/3'
-                                    title={data[2].attributes?.title}
-                                    description={data[2].attributes?.description}
-                                    href={data[2].attributes?.slug}
-                                />
-                            )}
-                            <div className='aspect-square 768:flex-1 relative'>
-                                <Image className='object-contain' src='/images/octopus.gif' alt='' fill={true} />
+                {data && (
+                    <Grid rowGap={2}>
+                        <Col mobileCols={2} tabletCols={8}>
+                            <div className='flex flex-col gap-8'>
+                                {data[0] && (
+                                    <DiaryCard
+                                        src={data[0].attributes?.cover?.data?.attributes?.url}
+                                        aspectRatio='16/9'
+                                        title={data[0].attributes?.title}
+                                        description={data[0].attributes?.description}
+                                        href={data[0].attributes?.slug}
+                                    />
+                                )}
+                                {data[1] && (
+                                    <DiaryCard
+                                        src={data[1].attributes?.cover?.data?.attributes?.url}
+                                        aspectRatio='16/9'
+                                        title={data[1].attributes?.title}
+                                        description={data[1].attributes?.description}
+                                        href={data[1].attributes?.slug}
+                                    />
+                                )}
                             </div>
-                        </div>
-                    </Col>
+                        </Col>
 
-                    <Col mobileCols={2} tabletCols={6}>
-                        <div className='flex flex-col gap-8'>
-                            {data[3] && (
-                                <DiaryCard
-                                    src={data[3].attributes?.cover?.data?.attributes?.url}
-                                    aspectRatio='16/9'
-                                    title={data[3].attributes?.title}
-                                    description={data[3].attributes?.description}
-                                    href={data[3].attributes?.slug}
-                                />
-                            )}
-                            {data[4] && (
-                                <DiaryCard
-                                    src={data[4].attributes?.cover?.data?.attributes?.url}
-                                    aspectRatio='4/3'
-                                    title={data[4].attributes?.title}
-                                    description={data[4].attributes?.description}
-                                    href={data[4].attributes?.slug}
-                                />
-                            )}
-                        </div>
-                    </Col>
+                        <Col mobileCols={2} tabletCols={4}>
+                            <div className='flex flex-col gap-8 h-full'>
+                                {data[2] && (
+                                    <DiaryCard
+                                        src={data[2].attributes?.cover?.data?.attributes?.url}
+                                        aspectRatio='4/3'
+                                        title={data[2].attributes?.title}
+                                        description={data[2].attributes?.description}
+                                        href={data[2].attributes?.slug}
+                                    />
+                                )}
+                                <div className='aspect-square 768:flex-1 relative'>
+                                    <Image className='object-contain' src='/images/octopus.gif' alt='' fill={true} />
+                                </div>
+                            </div>
+                        </Col>
 
-                    <Col mobileCols={2} tabletCols={6}>
-                        <div className='flex flex-col gap-8 h-full'>
-                            {data[5] && (
-                                <DiaryCard
-                                    src={data[5].attributes?.cover?.data?.attributes?.url}
-                                    aspectRatio='4/3'
-                                    title={data[5].attributes?.title}
-                                    description={data[5].attributes?.description}
-                                    href={data[5].attributes?.slug}
-                                />
-                            )}
-                            {data[6] && (
-                                <DiaryCard
-                                    src={data[6].attributes?.cover?.data?.attributes?.url}
-                                    aspectRatio='1/1'
-                                    title={data[6].attributes?.title}
-                                    description={data[6].attributes?.description}
-                                    href={data[6].attributes?.slug}
-                                />
-                            )}
-                        </div>
-                    </Col>
-                </Grid>
+                        <Col mobileCols={2} tabletCols={6}>
+                            <div className='flex flex-col gap-8'>
+                                {data[3] && (
+                                    <DiaryCard
+                                        src={data[3].attributes?.cover?.data?.attributes?.url}
+                                        aspectRatio='16/9'
+                                        title={data[3].attributes?.title}
+                                        description={data[3].attributes?.description}
+                                        href={data[3].attributes?.slug}
+                                    />
+                                )}
+                                {data[4] && (
+                                    <DiaryCard
+                                        src={data[4].attributes?.cover?.data?.attributes?.url}
+                                        aspectRatio='4/3'
+                                        title={data[4].attributes?.title}
+                                        description={data[4].attributes?.description}
+                                        href={data[4].attributes?.slug}
+                                    />
+                                )}
+                            </div>
+                        </Col>
+
+                        <Col mobileCols={2} tabletCols={6}>
+                            <div className='flex flex-col gap-8 h-full'>
+                                {data[5] && (
+                                    <DiaryCard
+                                        src={data[5].attributes?.cover?.data?.attributes?.url}
+                                        aspectRatio='4/3'
+                                        title={data[5].attributes?.title}
+                                        description={data[5].attributes?.description}
+                                        href={data[5].attributes?.slug}
+                                    />
+                                )}
+                                {data[6] && (
+                                    <DiaryCard
+                                        src={data[6].attributes?.cover?.data?.attributes?.url}
+                                        aspectRatio='1/1'
+                                        title={data[6].attributes?.title}
+                                        description={data[6].attributes?.description}
+                                        href={data[6].attributes?.slug}
+                                    />
+                                )}
+                            </div>
+                        </Col>
+                    </Grid>
+                )}
             </Container>
         </main>
     )
@@ -117,8 +119,6 @@ export async function getStaticProps(context) {
 
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL_DEV || process.env.NEXT_PUBLIC_API_URL}/posts?locale=${strapiLocale}&populate=*`)
     const data = await res.json()
-
-    console.log(data)
 
     return {
         props: data,
--- pages/nos.js
+++ pages/nos.js
@@ -1,4 +1,78 @@
+import ImageContainer from '@/components/atoms/ImageContainer'
+import AnimatedTitle from '@/components/molecules/AnimatedTitle'
+import Col from '@/components/styled-components/layout/Col'
+import Container from '@/components/styled-components/layout/Container'
+import Grid from '@/components/styled-components/layout/Grid'
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax'
+
 const Nos = () => {
-    return <div>Nos</div>
+    return (
+        <ParallaxProvider>
+            <main className='pb-16'>
+                <Container>
+                    <Grid>
+                        <Col desktopCols={9} offsetDesktop={3}>
+                            <p className='text-60 font-light'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </Col>
+                        <Col desktopCols={9}>
+                            <p className='text-60 mt-32 font-light'>Eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </Col>
+                    </Grid>
+                </Container>
+                <section className='mt-14'>
+                    <AnimatedTitle>aos olhos do diogo . aos olhos do diogo . aos olhos do diogo . </AnimatedTitle>
+                    <Container>
+                        <div className='relative'>
+                            <Grid>
+                                <Col desktopCols={8}>
+                                    <Parallax speed={15}>
+                                        <ImageContainer src='/images/her-image.png' aspectRatio='16/9' />
+                                    </Parallax>
+                                </Col>
+                                <Col desktopCols={4}>
+                                    <h2 className='font-power-grotesk text-90 leading-none'>Inês</h2>
+                                    <span className='default-text font-light'>Fotografia & Edição</span>
+                                </Col>
+                                <div className='w-1/2 absolute right-0 bottom-0'>
+                                    <p className='text-60 font-light'>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                                        aliqua.
+                                    </p>
+                                </div>
+                            </Grid>
+                        </div>
+                    </Container>
+                </section>
+                <section className='mt-14'>
+                    <AnimatedTitle>aos olhos da inês . aos olhos da inês . aos olhos da inês . </AnimatedTitle>
+                    <Container>
+                        <div className='relative'>
+                            <Grid>
+                                <Col desktopCols={4}>
+                                    <h2 className='font-power-grotesk text-90 leading-none'>Diogo</h2>
+                                    <span className='default-text font-light'>Vídeo & Edição</span>
+                                </Col>
+                                <Col desktopCols={8}>
+                                    <Parallax speed={5}>
+                                        <ImageContainer src='/images/him-image.png' aspectRatio='16/9' />
+                                    </Parallax>
+                                </Col>
+                                <div className='w-1/2 absolute left-0 top-80'>
+                                    <p className='text-60 font-light'>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+                                        <br />
+                                        <br />
+                                        Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    </p>
+                                </div>
+                            </Grid>
+                        </div>
+                    </Container>
+                </section>
+            </main>
+        </ParallaxProvider>
+    )
 }
 export default Nos
