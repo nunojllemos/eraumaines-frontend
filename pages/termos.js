5a173cb8783e02f17cf400564509b605608580f3
Nuno Lemos <nuno@v-a.pt>
2023-06-11 11:51:48 +0100

    Fix terms

--- pages/termos.js
+++ pages/termos.js
@@ -8,24 +8,28 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 const termos = ({ data }) => {
-    const { title, media, content } = data?.data?.attributes || {}
-    const { url } = media?.data?.attributes || {}
+    const { title, media, content } = data
+    const { url } = media.data.attributes || {}
 
     return (
         <main className='pb-16'>
-            <AnimatedTitle>{`${title} . ${title} . ${title} . ${title} .`}</AnimatedTitle>
+            {title && <AnimatedTitle>{`${title} . ${title} . ${title} . ${title} .`}</AnimatedTitle>}
             <Container>
                 <Grid tablet={800}>
-                    <Col tablet={800} mobileCols={2} tabletCols={6}>
-                        <div className='sticky top-8'>
-                            <ImageContainer src={getImage(url)} aspectRatio='1/1' sizes='(min-width: 800px) 50vw, 100vw' />
-                        </div>
-                    </Col>
-                    <Col tablet={800} mobileCols={2} tabletCols={6}>
-                        <div className='terms-content text-16 768:text-18'>
-                            <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
-                        </div>
-                    </Col>
+                    {url && (
+                        <Col tablet={800} mobileCols={2} tabletCols={6}>
+                            <div className='sticky top-8'>
+                                <ImageContainer src={getImage(url)} aspectRatio='1/1' sizes='(min-width: 800px) 50vw, 100vw' />
+                            </div>
+                        </Col>
+                    )}
+                    {content && (
+                        <Col tablet={800} mobileCols={2} tabletCols={6}>
+                            <div className='terms-content text-16 768:text-18'>
+                                <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+                            </div>
+                        </Col>
+                    )}
                 </Grid>
             </Container>
         </main>
@@ -35,19 +39,22 @@ export default termos
 
 export async function getStaticProps(context) {
     const { locale } = context
-    const populateQuery = 'populate=*'
-
     let strapiLocale
 
     if (locale === 'pt') strapiLocale = 'pt-PT'
     if (locale === 'en') strapiLocale = 'en'
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/term?locale=${strapiLocale}&${populateQuery}`)
+    const populateQuery = 'populate=*'
+    const baseApi = process.env.NEXT_PUBLIC_API_URL
+    const contentType = 'term'
+    const localeQuery = `locale=${strapiLocale}`
+
+    const res = await fetch(`${baseApi}/${contentType}?${localeQuery}&${populateQuery}`)
     const data = await res.json()
 
     return {
         props: {
-            data: data,
+            data: data.data.attributes,
         },
         revalidate: 10,
     }
