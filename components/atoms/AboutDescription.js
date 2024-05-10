e1d382cd0a7628f1d3dfb7fe2f3a4ae3aff01ae2
Nuno <nunojllemos@gmail.com>
2024-05-10 17:25:40 +0100

    fix: fix us page floating text

--- components/atoms/AboutDescription.js
+++ components/atoms/AboutDescription.js
@@ -1,4 +1,5 @@
 const AboutDescription = ({ description }) => {
-    return <p className='text-24 768:text-32 1280:text-50 font-light'>{description}</p>
+    // return <p className='text-24 768:text-32 1280:text-50 font-light'>{description}</p>
+    return <p className='relative 768:my-16 text-16 768:text-26 font-light'>{description}</p>
 }
 export default AboutDescription
--- components/organisms/AboutPersonSection.js
+++ components/organisms/AboutPersonSection.js
@@ -14,18 +14,21 @@ const AboutPersonSection = ({ isLeft, title, description, url, name, role }) => 
             <Container>
                 <div className='relative pb-4'>
                     <Grid>
-                        <Col className='row-start-2 768:row-start-1 my-12 768:my-0' mobileCols={2} tabletCols={8}>
+                        <Col className='z-0 row-start-2 1280:row-start-1 my-12 768:my-0' mobileCols={2} tabletCols={12} desktopCols={8}>
                             {url && (
                                 <MouseParallax strength={0.04}>
                                     <ImageContainer src={getImage(url)} aspectRatio='16/9' />
                                 </MouseParallax>
                             )}
                         </Col>
-                        <Col mobileCols={2} tabletCols={4}>
-                            <h2 className='font-power-grotesk text-50 768:text-70 1280:text-90 leading-none'>{name}</h2>
-                            <span className='default-text font-light'>{role}</span>
+                        <Col className='z-1' mobileCols={2} tabletCols={12} desktopCols={4}>
+                            <h2 className='relative font-power-grotesk text-50 768:text-70 1280:text-90 leading-none'>{name}</h2>
+                            <span className='relative default-text font-light'>{role}</span>
+                            <div className='hidden 1024:block'>
+                                <AboutDescription description={description} />
+                            </div>
                         </Col>
-                        <div className='col-span-2 768:w-3/4 1024:w-1/2 768:absolute right-0 top-44 1280:top-[unset] 1280:bottom-0'>
+                        <div className='1024:hidden col-span-2 768:col-span-12'>
                             <AboutDescription description={description} />
                         </div>
                     </Grid>
@@ -38,16 +41,19 @@ const AboutPersonSection = ({ isLeft, title, description, url, name, role }) => 
             <Container>
                 <div className='relative'>
                     <Grid>
-                        <Col mobileCols={2} tabletCols={4}>
-                            <h2 className='font-power-grotesk text-50 768:text-70 1280:text-90 leading-none'>{name}</h2>
-                            <span className='block default-text font-light'>{role}</span>
+                        <Col className='z-1' mobileCols={2} tabletCols={12} desktopCols={4}>
+                            <h2 className='relative font-power-grotesk text-50 768:text-70 1280:text-90 leading-none'>{name}</h2>
+                            <span className='relative block default-text font-light'>{role}</span>
+                            <div className='hidden 1024:block'>
+                                <AboutDescription description={description} />
+                            </div>
                         </Col>
-                        <Col className='my-12 768:my-0' mobileCols={2} tabletCols={8}>
+                        <Col className='z-0 my-12 768:my-0' mobileCols={2} tabletCols={12} desktopCols={8}>
                             <MouseParallax strength={0.04}>
                                 <ImageContainer src={getImage(url)} aspectRatio='16/9' />
                             </MouseParallax>
                         </Col>
-                        <div className='col-span-2 768:w-3/4 1024:w-1/2 768:absolute left-0 top-44 1280:bottom-0 1280:top-[unset]'>
+                        <div className='1024:hidden col-span-2 768:col-span-12'>
                             <AboutDescription description={description} />
                         </div>
                     </Grid>
